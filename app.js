08128939cb5683bbe072aad78dcbd586f063fa09
Luka Sharashenidze <lsharashenidze2001@gmail.com>
2025-03-05 17:20:25 +0400

    Update cors

--- app.js
+++ app.js
@@ -35,10 +35,7 @@ app.use(express.json());
 app.use(morgan("tiny"));
 app.use(
   cors({
-    origin:
-      process.env.NODE_ENV === "production"
-        ? "https://e-commerce-8gce.onrender.com"
-        : "http://localhost:5173",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
